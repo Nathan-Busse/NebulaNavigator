7c442bff1cdc814215fb0e36135b61df7fe85682
Nathan Busse <bussenathan@icloud.com>
2025-04-15 15:57:41 +0200

    unity?

---
+++ src/NebulaNavigator/Library/PackageCache/com.unity.ide.rider@7d01a2258732/Rider/Editor/EditorPluginInterop.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80e30d5227291deb1361da4e443875f20fc08f5236d0c53f11ec64998666b806
+size 6143
---
+++ src/NebulaNavigator/Library/PackageCache/com.unity.render-pipelines.core@54056c5186e5/Runtime/RenderGraph/Compiler/PassesData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae8e3b239102a019396d3db220a6efa16e066efec2bc43cf0e8c003230656b67
+size 55327
