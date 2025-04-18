287cbca8b93b1e9924119a29c47ac53237c35eb0
Nathan Busse <bussenathan@icloud.com>
2025-04-18 14:57:52 +0200

    implamented iframe

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10f50ef57b96e94c1c7bf190bed081934c6e9140d82d5f66de62c77f6d53fba2
-size 505
+oid sha256:2b7bf8ccc9c916f01bebecf9afddaa62fa21bc14389f65a428a1412f4a012cb2
+size 716
